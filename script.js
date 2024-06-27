ebf2235a45c6aef7354e2ab8be494bf80d0106f1
Maahir Jain <maahirdjain@gmail.com>
2024-06-28 03:10:49 +0530

    Create GameController object constructor

---
+++ script.js
@@ -0,0 +1,30 @@
+function GameController() {
+    let gameboard = ["", "", "", "", "", "", "", "", ""];
+    let currentPlayer = "X";
+}
+
+GameController.prototype.isMoveLegal = function(index) {
+    return gameboard[index] === "";
+}
+
+GameController.prototype.updateGameboard = function(index) {
+    gameboard[index] = currentPlayer;
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    return currentPlayer;
+}
+
+GameController.prototype.getWinner = function() {
+    let winnerString = "012,345,678,036,147,258,048,246";
+
+    let XString = "";
+    let OString = "";
+
+    for (let i = 0; i < gameboard.length; i++) {
+        if (gameboard[i] === "X") { XString += i; }
+        else if (gameboard[i] === "O") { OString += i; }
+    }
+
+    if (winnerString.contains(XString)) { return "X"; }
+    else if (winnerString.contains(OString)) { return "O"; }
+    else { return null; }
+}
