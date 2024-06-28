d882a2b0aa7d5d0a27c41fc9ef56cce48a08c4ae
Maahir Jain <maahirdjain@gmail.com>
2024-06-28 20:17:27 +0530

    Add play game functionality

--- script.js
+++ script.js
@@ -1,36 +1,32 @@
 function LogicController() {
-    let gameboard = ["", "", "", "", "", "", "", "", ""];
-    let currentPlayer = "X";
+    this.gameboard = ["", "", "", "", "", "", "", "", ""];
+    this.currentPlayer = "X";
 }
 
-GameController.prototype.resetGame = function() {
-    gameboard = ["", "", "", "", "", "", "", "", ""];
-    currentPlayer = "X";
+LogicController.prototype.resetGame = function() {
+    this.gameboard = ["", "", "", "", "", "", "", "", ""];
+    this.currentPlayer = "X";
 }
 
-GameController.prototype.getCurrentPlayer = function() {
-    return currentPlayer;
+LogicController.prototype.isMoveIllegal = function(index) {
+    return this.gameboard[index] !== "";
 }
 
-GameController.prototype.isMoveLegal = function(index) {
-    return gameboard[index] === "";
+LogicController.prototype.updateGameboard = function(index) {
+    this.gameboard[index] = this.currentPlayer;
+    this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
+    return this.currentPlayer;
 }
 
-GameController.prototype.updateGameboard = function(index) {
-    gameboard[index] = currentPlayer;
-    currentPlayer = currentPlayer === "X" ? "O" : "X";
-    return currentPlayer;
-}
-
-GameController.prototype.getWinnerAndString = function() {
+LogicController.prototype.getWinnerAndString = function() {
     const winnerStrings = ["012", "345", "678", "036", "147", "258", "048", "246"];
 
     let XString = "";
     let OString = "";
 
-    for (let i = 0; i < gameboard.length; i++) {
-        if (gameboard[i] === "X") { XString += i; }
-        else if (gameboard[i] === "O") { OString += i; }
+    for (let i = 0; i < this.gameboard.length; i++) {
+        if (this.gameboard[i] === "X") { XString += i; }
+        else if (this.gameboard[i] === "O") { OString += i; }
     }
 
     const winnerXStringIndex = winnerStrings.indexOf(XString);
@@ -46,18 +42,26 @@ GameController.prototype.getWinnerAndString = function() {
             winner: "O", 
             winnerString: winnerStrings[winnerOStringIndex] 
         }; 
-    } else { return { winner: null, winnerString: null }; }
+    } else { return null; }
 }
 
-GameController.prototype.getWinner = function() {
-    return getWinnerAndString.winner;
+LogicController.prototype.getWinner = function() {
+    return this.getWinnerAndString().winner;
 }
 
-GameController.prototype.getWinnerString = function() {
-    return getWinnerAndString.winnerString;
+LogicController.prototype.getWinnerString = function() {
+    return this.getWinnerAndString().winnerString;
+}
+
+LogicController.prototype.isWinner = function() {
+    return this.getWinnerAndString() !== null;
 }
 
 function DisplayController() {}
+
+DisplayController.prototype.targetToIndex = function(target) {
+    return target.dataset.index;
+}
 
 DisplayController.prototype.displayIllegal = function(target) {
     setTimeout(function(){
@@ -75,13 +79,25 @@ DisplayController.prototype.displayTurn = function(nextPlayer) {
     }
 }
 
-DisplayController.prototype.displayWin = function(winnerString) {
+DisplayController.prototype.displayWin = function(winner, winnerString) {
     for (let i = 0; i < winnerString.length; i++) {
         let gameBoxIndex = +winnerString.charAt(i);
         let gameBox = document.querySelector(`div[data-index=${gameBoxIndex}]`);
 
         gameBox.style.backgroundColor = "green";
     }
+
+    let img = document.querySelector("dialog img");
+
+    if (winner === "X") {
+        img.setAttribute("src", "./window-close.svg");
+        img.setAttribute("alt", "X icon");
+    } else {
+        img.setAttribute("src", "./circle-outline.svg");
+        img.setAttribute("alt", "O icon");
+    }
+
+    document.querySelector("dialog").showModal();
 }
 
 DisplayController.prototype.displayMark = function(target, currentPlayer) {
@@ -95,7 +111,55 @@ DisplayController.prototype.displayMark = function(target, currentPlayer) {
 DisplayController.prototype.displayReset = function() {
     this.displayTurn("X");
 
-    for(child of document.querySelectorAll(".grid")) {
+    for(child of Array.from(document.querySelectorAll(".grid div"))) {
         child.innerHTML = "";
     }
 }
+
+DisplayController.prototype.closeModal = function() {
+    document.querySelector("dialog").close();
+}
+
+function GameController() {
+    this.logicObj = new LogicController();
+    this.displayObj = new DisplayController();
+}
+
+GameController.prototype.gameBoxClicked = function(e) {
+    let target = e.target;
+
+    let index = this.displayObj.targetToIndex(target);
+    let currentPlayer = this.logicObj.currentPlayer;
+
+    if (this.logicObj.isMoveIllegal(index)) {
+        this.displayObj.displayIllegal(target);
+    } else {
+        this.displayObj.displayMark(target, currentPlayer);
+        let nextPlayer = this.logicObj.updateGameboard(index);
+
+        if (this.logicObj.isWinner) {
+            this.displayObj.displayWin
+            (this.logicObj.getWinner, this.logicObj.getWinnerString);
+        } else {
+            this.displayObj.displayTurn(nextPlayer);
+        }
+    }
+}
+
+GameController.prototype.playGame = function() {
+    for(child of Array.from(document.querySelectorAll(".grid div"))) {
+        child.addEventListener("click", (e) => this.gameBoxClicked(e));
+    }
+
+    document.querySelector("#play-again").addEventListener("click", () => {
+        this.displayObj.closeModal();
+        this.displayObj.displayReset();
+    })
+
+    document.querySelector("#back").addEventListener("click", () => {
+        this.displayObj.closeModal();
+    })
+}
+
+let gameObj = new GameController();
+gameObj.playGame();
