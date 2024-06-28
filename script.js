4c160b18d84c6cf3a3f09528058f3fd478c7bcf4
Maahir Jain <maahirdjain@gmail.com>
2024-06-28 19:07:05 +0530

    Create DisplayController object constructor

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function GameController() {
+function LogicController() {
     let gameboard = ["", "", "", "", "", "", "", "", ""];
     let currentPlayer = "X";
+}
+
+GameController.prototype.resetGame = function() {
+    gameboard = ["", "", "", "", "", "", "", "", ""];
+    currentPlayer = "X";
+}
+
+GameController.prototype.getCurrentPlayer = function() {
+    return currentPlayer;
 }
 
 GameController.prototype.isMoveLegal = function(index) {
@@ -13,8 +22,8 @@ GameController.prototype.updateGameboard = function(index) {
     return currentPlayer;
 }
 
-GameController.prototype.getWinner = function() {
-    let winnerString = "012,345,678,036,147,258,048,246";
+GameController.prototype.getWinnerAndString = function() {
+    const winnerStrings = ["012", "345", "678", "036", "147", "258", "048", "246"];
 
     let XString = "";
     let OString = "";
@@ -24,8 +33,69 @@ GameController.prototype.getWinner = function() {
         else if (gameboard[i] === "O") { OString += i; }
     }
 
-    if (winnerString.contains(XString)) { return "X"; }
-    else if (winnerString.contains(OString)) { return "O"; }
-    else { return null; }
+    const winnerXStringIndex = winnerStrings.indexOf(XString);
+    const winnerOStringIndex = winnerStrings.indexOf(OString);
+
+    if (winnerXStringIndex !== -1) { 
+        return { 
+            winner: "X", 
+            winnerString: winnerStrings[winnerXStringIndex] 
+        }; 
+    } else if (winnerOStringIndex !== -1) { 
+        return { 
+            winner: "O", 
+            winnerString: winnerStrings[winnerOStringIndex] 
+        }; 
+    } else { return { winner: null, winnerString: null }; }
 }
 
+GameController.prototype.getWinner = function() {
+    return getWinnerAndString.winner;
+}
+
+GameController.prototype.getWinnerString = function() {
+    return getWinnerAndString.winnerString;
+}
+
+function DisplayController() {}
+
+DisplayController.prototype.displayIllegal = function(target) {
+    setTimeout(function(){
+        target.style.backgroundColor = "red";
+   },1000);
+}
+
+DisplayController.prototype.displayTurn = function(nextPlayer) {
+    let yourTurnText = document.querySelector(".content p:first-child");
+
+    if (nextPlayer === "X") {
+        yourTurnText.innerHTML = "Your turn: <img src=\"./window-close.svg\" alt=\"X icon\">";
+    } else {
+        yourTurnText.innerHTML = "Your turn: <img src=\"./circle-outline.svg\" alt=\"O icon\">";
+    }
+}
+
+DisplayController.prototype.displayWin = function(winnerString) {
+    for (let i = 0; i < winnerString.length; i++) {
+        let gameBoxIndex = +winnerString.charAt(i);
+        let gameBox = document.querySelector(`div[data-index=${gameBoxIndex}]`);
+
+        gameBox.style.backgroundColor = "green";
+    }
+}
+
+DisplayController.prototype.displayMark = function(target, currentPlayer) {
+    if (currentPlayer === "X") {
+        target.innerHTML = "<img src=\"./window-close.svg\" alt=\"X icon\">";
+    } else {
+        target.innerHTML = "<img src=\"./circle-outline.svg\" alt=\"O icon\">";
+    }
+}
+
+DisplayController.prototype.displayReset = function() {
+    this.displayTurn("X");
+
+    for(child of document.querySelectorAll(".grid")) {
+        child.innerHTML = "";
+    }
+}
